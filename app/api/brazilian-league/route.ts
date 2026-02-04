4b768d632ff427927c419e6a72ade553238e0711
bruno.cesar <bruno.cesar@dtidigital.com.br>
2026-02-03 23:16:38 -0300

    #feat: Adjust brazilian-league route.

--- app/api/brazilian-league/route.ts
+++ app/api/brazilian-league/route.ts
@@ -1,33 +1,95 @@
-import { getBrazilianLeagueByGloboEsporte } from '@/services/brazuerao.service'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import axios, { AxiosRequestConfig } from 'axios'
 import { NextRequest, NextResponse } from 'next/server'
+
+const URL_BASE_GLOBOESPORTE = 'http://globoesporte.globo.com'
+const USER_AGENT =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
+
+const defaultOptions: AxiosRequestConfig = {
+  baseURL: URL_BASE_GLOBOESPORTE,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+
+const apiBrasileiraoGloboEsporte = axios.create(defaultOptions)
+
+apiBrasileiraoGloboEsporte.interceptors.request.use(
+  function (config) {
+    config.headers.setUserAgent(USER_AGENT)
+    return config
+  },
+  function (error) {
+    console.error(JSON.stringify(error))
+    return Promise.reject(error)
+  }
+)
 
 export async function GET(request: NextRequest) {
   try {
-    const data = await getBrazilianLeagueByGloboEsporte()
-    return Response.json(data, {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://brazuerao-betting.vercel.app',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
+    const { data } = await apiBrasileiraoGloboEsporte.get(
+      '/futebol/brasileirao-serie-a',
+      { responseType: 'document' }
+    )
+
+    const regex = /const\s+classificacao\s+=\s+({.*?});/
+    const match = regex.exec(data)
+
+    if (!match) throw new Error('Não foi possível encontrar a tabela EXDD')
+
+    const classificacaoJSON = match[1]
+    const allData = JSON.parse(classificacaoJSON)
+
+    const { classificacao } = allData
+
+    const table = classificacao.map((team: any) => {
+      return {
+        position: team.ordem,
+        played: team.jogos,
+        name: team.nome_popular,
+        shield: team.escudo,
+      }
     })
+
+    if (!table || table.length === 0)
+      return NextResponse.json(
+        { message: 'No data found' },
+        {
+          status: 404,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*', // or specific origin
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+        }
+      )
+
+    return NextResponse.json(table)
   } catch (error) {
-    console.error('Erro ao buscar classificação:', error)
+    console.error(error)
     return NextResponse.json(
-      { error: 'Falha ao buscar classificação' },
-      { status: 500 }
+      { message: 'Internal Error', Error: (error as Error).message },
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*', // or specific origin
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+      }
     )
   }
 }
 
-// Handle preflight OPTIONS request
 export async function OPTIONS(request: Request) {
   return new Response(null, {
     status: 200,
     headers: {
-      'Access-Control-Allow-Origin': 'https://brazuerao-betting.vercel.app',
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'api.sofascore.com' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'e1.pxfuel.com' },
+      { protocol: 'https', hostname: 'img.freepik.com' },
+      { protocol: 'https', hostname: 'api.sofascore.com' }
+    ],
     unoptimized: process.env.NODE_ENV === 'development',
   },
 }
